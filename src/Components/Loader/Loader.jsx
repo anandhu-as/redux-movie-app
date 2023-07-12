d14b168626c14d520d828f61bed1908e5ed73aba
anandhu-as <anandhuas010203@gmail.com>
2023-07-12 13:26:26 +0530

    added loding

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "../../styles/Loader.css";
+const Loader = (UpdatedComponent) => {
+  return (props) => {
+    const [loading, setLoading] = useState(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+    return (
+      <div>
+        {loading ? (
+          <svg viewBox="25 25 50 50">
+            <circle r="20" cy="50" cx="50"></circle>
+          </svg>
+        ) : (
+          <UpdatedComponent {...props} />
+        )}
+      </div>
+    );
+  };
+};
+
+export default Loader;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "../Components/NavBar/NavBar";
 import Home from "../Components/Home/Home";
 import WatchList from "../Components/WatchList/WatchList";
+import About from "../pages/About";
+import Loader from "../Components/Loader/Loader";
+const MovieLoader=Loader(Home)
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<MovieLoader/>} />
           <Route path="/watchlist" element={<WatchList />} />
+          <Route path="/about" element={<About/>}/>
         </Routes>
       </BrowserRouter>
     </div>
